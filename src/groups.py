7e161e68accaeba492fe6dfceaa02b39ee69c490
AlbertQmoa <lovesphysics@gmail.com>
2024-02-16 19:25:34 +0800

    Check the validation of a group

--- src/groups.py
+++ src/groups.py
@@ -5,10 +5,12 @@ class Group:
         # cayley_table is a list of list, where cayley_table[0] was assumed to be {e, ...}
         self.cayley_table = self._sort_cayley_table_by_index(cayley_table)
         self.g = cayley_table[0]
-        self._col = cayley_table[0]
+        self.order = len(cayley_table[0])
+        self.mult = self._create_gi_multiply_gj(cayley_table)
+        self._check_validation_of_cayley_table()
 
     def __str__(self):
-        result = "   " + "  ".join(self._col) + "\n"
+        result = "   " + "  ".join(self.g) + "\n"
         for row in self.cayley_table:
             row_str = row[0] + "  " + "  ".join(str(elem) for elem in row)
             result += row_str + "\n"
@@ -27,14 +29,49 @@ class Group:
                 raise ValueError(f'Cannot find {g} in the index of cayley_table.')
         return sorted_caley_table
 
-    def are_rearranged_from_the_group_elements(self, array):
-        pass
-
-
-    def _is_cayley_table_validated(self):
-        # Check the validation of the cayley table
-        return True
+    def are_rearranged_from_the_group_elements(self, a_list):
+        return sorted(a_list) == sorted(self.g)
     
+    def get_index_of_gi(self, gi):
+        for i in range(self.order):
+            if self.g[i] == gi:
+                return i
+        raise ValueError(f'Cannot find index for {gi}')
+
+    def _create_gi_multiply_gj(self, cayley_table):
+        mult = dict()
+        order = len(cayley_table[0])
+        for i in range(order):
+            for j in range(order):
+                gi = cayley_table[i][0]
+                gj = cayley_table[0][j]
+                gigj = f'{gi}*{gj}'
+                mult[gigj] = cayley_table[i][j]
+                idx = self.get_index_of_gi(gi)
+                mult[f'{idx}*{j}'] = str(self.get_index_of_gi(mult[gigj]))
+        return mult
+
+    def _check_validation_of_cayley_table(self):
+        # --- Check the order ---
+        if len(self.cayley_table) != self.order:
+            raise ValueError('cayley_table is not n * n')
+        # --- Check rearrangement ---
+        for row in self.cayley_table:
+            if not self.are_rearranged_from_the_group_elements(row):
+                raise ValueError(f'j direction: {row} is not rearranged from G')
+        for j in range(self.order):
+            row = [self.cayley_table[i][j] for i in range(self.order)]
+            if not self.are_rearranged_from_the_group_elements(row):
+                raise ValueError(f'i direction: {row} is not rearranged from G')
+        # --- Check associative law ---
+        for i in range(self.order):
+            for j in range(self.order):
+                for k in range(self.order):
+                    ij = self.mult[f'{i}*{j}']
+                    jk = self.mult[f'{j}*{k}']
+                    if self.mult[f'{ij}*{k}'] != self.mult[f'{i}*{jk}']:
+                        raise ValueError(f'{self.g[i]}*{self.g[j]}*{self.g[k]} violates the assicuative law')
+
     def find_inverse(self, element):
         # 查找并返回给定元素的逆元
         pass
@@ -57,3 +94,4 @@ if __name__ == '__main__':
     ]
     C4 = Group(cayley)
     print(C4)
+    print(C4.mult)
--- test/test_groups.py
+++ test/test_groups.py
@@ -7,7 +7,7 @@ import pytest
 
 class TestGroup:
     zero_torelance = 1e-10
-    
+
     @pytest.fixture(scope='class')
     def get_groups(self):
         cayley_table_C3 = [
@@ -21,14 +21,36 @@ class TestGroup:
             ['a', 'b', 'c', 'e'],
             ['c', 'e', 'a', 'b']    
         ]
-        C3 = Group(cayley_table_C3)
-        C4 = Group(cayley_table_C4)
-        return [C3, C4]
+        groups = {
+            'C3': Group(cayley_table_C3),
+            'C4': Group(cayley_table_C4)
+        }
+        return groups
 
     def test_sort_cayley_table_by_index(self, get_groups):
-        for G in get_groups:
+        for _, G in get_groups.items():
             index = [row[0] for row in G.cayley_table]
             assert index == G.cayley_table[0]
 
+    def test_are_rearranged_from_the_group_elements(self, get_groups):
+        C4 = get_groups['C4']
+        assert C4.are_rearranged_from_the_group_elements(['a', 'b', 'c', 'e'])
+        assert not C4.are_rearranged_from_the_group_elements(['a', 'a', 'c', 'e'])
+    
+    def test_get_index_of_gi(self, get_groups):
+        for _, G in get_groups.items():
+            for i in range(G.order):
+                assert G.get_index_of_gi(G.g[i]) == i
+
+    def test_create_gi_multiply_gj(self, get_groups):
+        C4 = get_groups['C4']
+        assert C4.mult['a*e'] == 'a' and C4.mult['e*a'] == 'a'
+        assert C4.mult['a*a'] == 'b'
+        assert C4.mult['a*b'] == 'c' and C4.mult['b*a'] == 'c'
+        assert C4.mult['a*c'] == 'e' and C4.mult['c*a'] == 'e'
+        assert C4.mult['1*0'] == '1' and C4.mult['0*1'] == '1'
+        assert C4.mult['1*1'] == '2'
+        assert C4.mult['1*2'] == '3' and C4.mult['2*1'] == '3'
+        assert C4.mult['1*3'] == '0' and C4.mult['3*1'] == '0'
 
 
